879aee4676aa4b4c1286cc07f61e248291177f61
jiayouzhao <jiayouzhaodaye@163.com>
2021-05-04 02:03:15 +0800

    添加html、css、js

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-canvas.onmousedown = ()=>{}
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
+
+ctx.fillStyle = 'black';
+ctx.strokeStyle = "black";
+ctx.lineWidth = 4;
+ctx.lineCap = "round";
+
+function paintLine (x1,y1,x2,y2){
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
+
+
+let painting = false;
+let oldpoint ;
+let touchTurn = "ontouchstart" in document.documentElement;
+
+if(touchTurn){
+
+    canvas.ontouchstart = (e)=>{
+        let x = e.touches[0].clientX;
+        let y = e.touches[0].clientY;
+        oldpoint = [x,y];
+    }
+
+    canvas.ontouchmove = (e)=>{
+        let x = e.touches[0].clientX;
+        let y = e.touches[0].clientY;
+    
+        ctx.beginPath();
+        paintLine(oldpoint[0],oldpoint[1],x,y);
+        oldpoint = [x,y]
+    }
+
+} else {
+
+    canvas.onmousedown = (e)=>{
+        painting = true;
+        oldpoint = [e.clientX,e.clientY];
+    }
+
+    canvas.onmousemove = (e)=>{
+        if(painting === true){
+            ctx.beginPath();
+            paintLine(oldpoint[0],oldpoint[1],e.clientX,e.clientY);
+            oldpoint = [e.clientX,e.clientY]
+        }
+        
+    }
+
+    canvas.onmouseup = ()=>{
+        painting = false;
+    }
+
+}
